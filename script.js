05d3e3e176b17106b0a9ebce3b078aa7811e5dc9
NichWes <90470583+NichWes@users.noreply.github.com>
2024-10-03 16:49:17 +0700

    membuat our cakes section

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const searchIcon = document.querySelector('.search-icon');
-const searchForm = document.querySelector('.search-form');
+const searchIcon = document.querySelector(".search-icon");
+const searchForm = document.querySelector(".search-form");
 
-searchIcon.addEventListener('click', () => {
-  searchForm.classList.add('active');
-  cartItemsContainer.classList.remove('active');
+searchIcon.addEventListener("click", () => {
+  searchForm.classList.add("active");
+  cartItemsContainer.classList.remove("active");
 });
 
-const cartIcon = document.querySelector('.cart-icon');
-const cartItemsContainer = document.querySelector('.cart-items-container');
+const cartIcon = document.querySelector(".cart-icon");
+const cartItemsContainer = document.querySelector(".cart-items-container");
 
-cartIcon.addEventListener('click', () => {
-  cartItemsContainer.classList.add('active');
-  searchForm.classList.remove('active');
+cartIcon.addEventListener("click", () => {
+  cartItemsContainer.classList.add("active");
+  searchForm.classList.remove("active");
 });
